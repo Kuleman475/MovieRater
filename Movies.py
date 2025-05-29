d1ce0657364d1db12905e5b74a04c213bf085fca
Taylor Christensen <chirstensentaylor6@gmail.com>
2025-05-28 21:24:56 -0400

    where

--- Movies.py
+++ Movies.py
@@ -2,5 +2,5 @@ import sqlite3
 
 con = sqlite3.connect("Movies.db")
 cur = con.cursor()
-for row in cur.execute('SELECT * FROM movies ORDER BY stars'):
+for row in cur.execute('SELECT * FROM movies WHERE stars = 5'):
     print(row)
